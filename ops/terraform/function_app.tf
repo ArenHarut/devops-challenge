e0aa230cb21fb44313805429e1e477fb388b8677
Aren Harutyunyan <aren.harutyunyan@lineate.com>
2024-10-06 17:46:48 +0400

    adding changes to fork

--- ops/terraform/function_app.tf
+++ ops/terraform/function_app.tf
@@ -1,3 +1,37 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "main-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "linuxfunctionappsa"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "main-app-service-plan"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+locals {
+  resource_prefix = "approvalmax"
+  default_tags = {
+    app = "streaming"
+    tier = "business"
+    dept = "R&D"
+  }
+}
+
 resource "azurerm_linux_function_app" "main" {
   name                       = "${local.resource_prefix}-realtime-processing-service"
   resource_group_name        = azurerm_resource_group.main.name
